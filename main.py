a7cc5fb6c80352d4fc06add831452f421007cb6e
jpboonie <jpboonie@gmail.com>
2021-03-15 20:32:58 +0000

    four

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ y='john'
 print(x)
 print(y)
 
+#question 1
 print('jacob')
 print("""Here comes my happiness again
 Right back to where it should have been
@@ -45,3 +46,31 @@ I just wanna dance the night away
 With senoritas who can sway
 Right now tomorrow's lookin' bright
 Just like the sunny mornin' light""")
+
+#question 2
+print("1 2 3 4 5 6 7")
+print(10 + 7)
+x=7
+y=10
+print(x+y)
+
+#question 3
+x='hannah'
+print(x)
+s = "My lucky number is %d, what is yours?" % 7
+print(s[3:8])
+s = "The date is %d/%d/%d" % (7, 7, 2016)
+print(s)
+
+#Question 4
+import random 
+
+x = random.randrange(0,10)
+print(x)
+
+import random as r
+
+print(r.randrange(0,10))
+print(r.randrange(0,10))
+print(r.randrange(0,10))
+
